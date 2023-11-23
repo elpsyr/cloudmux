f0bc4aa4a312a356ac959a1c352c020ae3e5009a
he11c <hellccqcq@gmail.com>
2023-11-23 10:39:57 +0800

    feat(root): qcloud test

--- pkg/multicloud/qcloud/instancetype.go
+++ pkg/multicloud/qcloud/instancetype.go
@@ -15,6 +15,8 @@
 package qcloud
 
 import (
+	"time"
+	"yunion.io/x/cloudmux/pkg/cloudprovider"
 	"yunion.io/x/log"
 	"yunion.io/x/pkg/errors"
 	"yunion.io/x/pkg/utils"
@@ -25,6 +27,9 @@ import (
 // {"CpuCoreCount":1,"EniQuantity":1,"GPUAmount":0,"GPUSpec":"","InstanceTypeFamily":"ecs.t1","InstanceTypeId":"ecs.t1.xsmall","LocalStorageCategory":"","MemorySize":0.500000}
 // InstanceBandwidthRx":26214400,"InstanceBandwidthTx":26214400,"InstancePpsRx":4500000,"InstancePpsTx":4500000
 
+// Verify that *SInstanceType implements ICloudSku interface
+var _ cloudprovider.ICloudSku = (*SInstanceType)(nil)
+
 type SInstanceType struct {
 	Zone              string //	可用区。
 	InstanceType      string //	实例机型。
@@ -34,6 +39,151 @@ type SInstanceType struct {
 	Memory            int    //	内存容量，单位：GB。
 	CbsSupport        string //	是否支持云硬盘。取值范围：TRUE：表示支持云硬盘；FALSE：表示不支持云硬盘。
 	InstanceTypeState string //	机型状态。取值范围：AVAILABLE：表示机型可用；UNAVAILABLE：表示机型不可用。
+}
+
+func (self *SInstanceType) GetId() string {
+	return self.InstanceType
+}
+
+func (self *SInstanceType) GetName() string {
+	return self.InstanceType
+}
+
+func (self *SInstanceType) GetGlobalId() string {
+	return self.InstanceType
+}
+
+func (self *SInstanceType) GetCreatedAt() time.Time {
+
+	return time.Now()
+}
+
+func (self *SInstanceType) GetDescription() string {
+	return ""
+}
+
+func (self *SInstanceType) GetStatus() string {
+	return ""
+}
+
+func (self *SInstanceType) Refresh() error {
+	return nil
+}
+
+func (self *SInstanceType) IsEmulated() bool {
+	return false
+}
+
+func (self *SInstanceType) GetSysTags() map[string]string {
+	return nil
+}
+
+func (self *SInstanceType) GetTags() (map[string]string, error) {
+	return nil, nil
+}
+
+func (self *SInstanceType) SetTags(tags map[string]string, replace bool) error {
+	return nil
+}
+
+func (self *SInstanceType) GetZoneID() string {
+	return ""
+}
+
+func (self *SInstanceType) GetInstanceTypeFamily() string {
+	return ""
+}
+
+func (self *SInstanceType) GetInstanceTypeCategory() string {
+	return ""
+}
+
+func (self *SInstanceType) GetPrepaidStatus() string {
+	return ""
+}
+
+func (self *SInstanceType) GetPostpaidStatus() string {
+	return ""
+}
+
+func (self *SInstanceType) GetCpuArch() string {
+	return ""
+}
+
+func (self *SInstanceType) GetCpuCoreCount() int {
+	return self.CPU
+}
+
+func (self *SInstanceType) GetMemorySizeMB() int {
+	return int(self.Memory * 1024)
+}
+
+func (self *SInstanceType) GetOsName() string {
+	return ""
+}
+
+func (self *SInstanceType) GetSysDiskResizable() bool {
+	return false
+}
+
+func (self *SInstanceType) GetSysDiskType() string {
+	return ""
+}
+
+func (self *SInstanceType) GetSysDiskMinSizeGB() int {
+	return 0
+}
+
+func (self *SInstanceType) GetSysDiskMaxSizeGB() int {
+	return 0
+}
+
+func (self *SInstanceType) GetAttachedDiskType() string {
+	return ""
+}
+
+func (self *SInstanceType) GetAttachedDiskSizeGB() int {
+	return 0
+}
+
+func (self *SInstanceType) GetAttachedDiskCount() int {
+	return 0
+}
+
+func (self *SInstanceType) GetDataDiskTypes() string {
+	return ""
+}
+
+func (self *SInstanceType) GetDataDiskMaxCount() int {
+	return 0
+}
+
+func (self *SInstanceType) GetNicType() string {
+	return ""
+}
+
+func (self *SInstanceType) GetNicMaxCount() int {
+	return 0
+}
+
+func (self *SInstanceType) GetGpuAttachable() bool {
+	return false
+}
+
+func (self *SInstanceType) GetGpuSpec() string {
+	return ""
+}
+
+func (self *SInstanceType) GetGpuCount() string {
+	return ""
+}
+
+func (self *SInstanceType) GetGpuMaxCount() int {
+	return 0
+}
+
+func (self *SInstanceType) Delete() error {
+	return nil
 }
 
 func (self *SRegion) GetInstanceTypes() ([]SInstanceType, error) {
@@ -127,4 +277,16 @@ func (self *SRegion) GetZoneLocalStorages(zoneId string) ([]string, error) {
 		}
 	}
 	return storages, nil
+}
+
+func (self *SRegion) GetISkus() ([]cloudprovider.ICloudSku, error) {
+	skus, err := self.GetInstanceTypes()
+	if err != nil {
+		return nil, errors.Wrapf(err, "GetInstanceTypes")
+	}
+	ret := []cloudprovider.ICloudSku{}
+	for i := range skus {
+		ret = append(ret, &skus[i])
+	}
+	return ret, nil
 }

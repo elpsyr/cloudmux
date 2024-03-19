d46a3ff06868d5abcafd7e768180b3205e82f35a
he11c <hellccqcq@gmail.com>
2024-03-19 19:48:31 +0800

    feat(cfel): cloudpods  implement region GetIVMs
    Signed-off-by: he11c <hellccqcq@gmail.com>

--- pkg/multicloud/cloudpods/region_cfel.go
+++ pkg/multicloud/cloudpods/region_cfel.go
@@ -29,3 +29,17 @@ func (self *SRegion) GetPostPaidStatus(zoneID, instanceType string) (string, err
 func (self *SRegion) GetPrePaidStatus(zoneID, instanceType string) (string, error) {
 	return "", cloudprovider.ErrNotImplemented
 }
+
+// GetIVMs
+// region 返回的 vm 无 host *SHost 操作对象
+func (self *SRegion) GetIVMs() ([]cloudprovider.ICloudVM, error) {
+	instances, err := self.GetInstances("")
+	if err != nil {
+		return nil, err
+	}
+	var ret []cloudprovider.ICloudVM
+	for i := range instances {
+		ret = append(ret, &instances[i])
+	}
+	return ret, nil
+}

8dbfec70b8f512de461405fca8bc515b72143209
zhaeng <zhangfeng@chengfengerlai.com>
2024-03-20 17:38:48 +0800

    feat(cfel):add security group rule

--- pkg/multicloud/cloudpods/secgroup_cfel.go
+++ pkg/multicloud/cloudpods/secgroup_cfel.go
@@ -1,6 +1,10 @@
 package cloudpods
 
-import modules "yunion.io/x/onecloud/pkg/mcclient/modules/compute"
+import (
+	"yunion.io/x/cloudmux/pkg/cloudprovider"
+	modules "yunion.io/x/onecloud/pkg/mcclient/modules/compute"
+	api "yunion.io/x/onecloud/pkg/apis/compute"
+)
 
 func (self *SRegion) CfelGetSecurityGroups() ([]SSecurityGroup, error) {
 	params := map[string]interface{}{
@@ -8,4 +12,24 @@ func (self *SRegion) CfelGetSecurityGroups() ([]SSecurityGroup, error) {
 	}
 	ret := []SSecurityGroup{}
 	return ret, self.cli.list(&modules.SecGroups, params, &ret)
+}
+
+func (self *SSecurityGroup) CreateRule(opts *cloudprovider.SecurityGroupRuleCreateOptions) (cloudprovider.ISecurityGroupRule, error) {
+	return self.region.CfelCreateSecRule(self.Id,opts)
+}
+
+func (self *SRegion) CfelCreateSecRule(secId string, opts *cloudprovider.SecurityGroupRuleCreateOptions) (cloudprovider.ISecurityGroupRule, error) {
+	input := api.SSecgroupRuleCreateInput{}
+	input.SecgroupId = secId
+	input.Priority = &opts.Priority
+	input.Action = string(opts.Action)
+	input.Protocol = string(opts.Protocol)
+	input.Direction = string(opts.Direction)
+	input.Description = opts.Desc
+	input.CIDR = opts.CIDR
+
+	input.Ports = opts.Ports
+	// ret := struct{}{}
+	var ret = &SecurityGroupRule{region: self}
+	return ret,self.create(&modules.SecGroupRules, input, &ret)
 }
